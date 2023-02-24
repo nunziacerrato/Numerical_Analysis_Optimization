102f7520db5265c4814e0815ec8e3137d7464b01
Nunzia Cerrato <cerratonunzia@gmail.com>
2023-02-24 19:48:27 +0100

    minor changes

--- Project_1/Project_1.py
+++ Project_1/Project_1.py
@@ -3,8 +3,36 @@
 import numpy as np
 import scipy.linalg
 import logging
+import tenpy
+import qutip
 
 logging.basicConfig(level=logging.DEBUG)
+
+
+
+def create_dataset(num_matr,dim_matr):
+    ''' '''
+
+    Random = np.zeros((num_matr,dim_matr,dim_matr))
+    Ginibre = np.zeros((num_matr,dim_matr,dim_matr))
+    GOE = np.zeros((num_matr,dim_matr,dim_matr))
+    GUE = np.zeros((num_matr,dim_matr,dim_matr))
+    Wishart = np.zeros((num_matr,dim_matr,dim_matr))
+    # Diag_domin = np.zeros((num_matr,dim_matr,dim_matr))
+    # Hilbert = np.zeros((num_matr,dim_matr,dim_matr))
+
+    for i in range(num_matr):
+        
+        Random[i,:,:] = np.random.rand(dim_matr,dim_matr)
+        Ginibre[i,:,:] = np.random.normal(loc=0.0, scale=1.0, size=(dim_matr,dim_matr)) + \
+                         1j*np.random.normal(loc=0.0, scale=1.0, size=(dim_matr,dim_matr))
+        GOE[i,:,:] = tenpy.linalg.random_matrix.GOE((dim_matr,dim_matr))
+        GUE[i,:,:] = tenpy.linalg.random_matrix.GUE((dim_matr,dim_matr))
+        Wishart[i,:,:] = np.array(qutip.rand_dm_ginibre((dim_matr), rank=None))
+        # Hilbert[i,]
+        # A = np.array(qutip.rand_dm_ginibre((dim_matr), rank=None))
+
+    return Random, Ginibre, GOE, GUE, Wishart
 
 def lufact(A):
     r''' This function computes the LU factorization of a non-singular (?) matrix A
@@ -50,11 +78,42 @@ def lufact(A):
     g = np.amax(LU_abs)/np.amax(np.abs(A))
     return L, U, g
 
-A = np.array([[1,2,3],[4,5,6],[7,8,9]])
-# print(np.abs(A))
-# print(np.amax(np.abs(A)))
-L, U, g = lufact(A)
-print('g=',g)
-print('L=',L)
-print('U=',U)
-# print(L@U)
+def relative_backward_error(A,L,U):
+    ''' '''
+    return np.linalg.norm(A - L@U, ord=inf)/np.linalg.norm(A, ord=inf)
+
+def stat_analysis(matr_vector):
+    ''' '''
+    
+    number_matrices = matr_vector.shape[0]
+    for i in range(number_matrices):
+        # Random, Ginibre, GOE, GUE, Wishart = create_dataset(num_matr, dim_matr)
+        matrix = matr_vector[i]
+        L, u, g = lufact(matrix)
+        rel_back_error = relative_backward_error(matrix,L,U)
+
+
+    return
+
+if __name__ == '__main__':
+    
+    num_matr = 100
+    dim_matr = 10
+
+    # Create the dataset
+    Random, Ginibre, GOE, GUE, Wishart = create_dataset(num_matr,dim_matr)
+    dataset = [Random, Ginibre, GOE, GUE, Wishart]
+
+    # Statistics associated to the dataset
+    mean, devst = stat_analysis(dataset) # ciclo for, aggiustare
+
+
+    ################################################################################################
+    A = np.array([[1,2,3],[4,5,6],[7,8,9]])
+    # print(np.abs(A))
+    # print(np.amax(np.abs(A)))
+    L, U, g = lufact(A)
+    print('g=',g)
+    print('L=',L)
+    print('U=',U)
+    # print(L@U)
---
+++ test/test_Project1.py
@@ -0,0 +1,17 @@
+""" In this program the most important functions of the library are tested.
+"""
+import numpy as np
+import unittest
+from Project_1.Project_1 import *
+
+
+class TestProject1(unittest.TestCase):
+
+    def test1(self):
+        ''' docstring '''
+        
+
+
+
+if __name__ == '__main__':
+    unittest.main()
