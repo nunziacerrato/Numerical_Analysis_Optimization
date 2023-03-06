020315f882a07ab7475d6edd31cfe975a29f3b38
Nunzia Cerrato <cerratonunzia@gmail.com>
2023-03-06 15:50:16 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ['qutip']
+autodoc_mock_imports = ['pandas','qutip']
 
 
 # -- Options for HTML output -------------------------------------------------
